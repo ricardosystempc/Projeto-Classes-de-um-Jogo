4a61cac7d1f004b8cac37de43f21368e8ede3bf9
Ricardo Paixão <84331376+ricardosystempc@users.noreply.github.com>
2026-02-25 08:40:50 -0300

    Index.js
    Enviando o Index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const Heroi = require("./Heroi");
+
+const heroi1 = new Heroi("Merlin", 150, "mago");
+const heroi2 = new Heroi("Arthas", 30, "guerreiro");
+const heroi3 = new Heroi("Lee", 40, "monge");
+const heroi4 = new Heroi("Hanzo", 28, "ninja");
+
+heroi1.atacar();
+heroi2.atacar();
+heroi3.atacar();
+heroi4.atacar();
